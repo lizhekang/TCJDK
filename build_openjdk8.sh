95439c9e68e4c6bc2a91e85284fc5cb044a1f317
dalex <lizhekang@hotmail.com>
2016-03-25 01:41:40 -0700

    Setting ENV.

--- build_openjdk8.sh
+++ build_openjdk8.sh
@@ -4,3 +4,8 @@ export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
 export PATH=$JAVA_HOME/bin:$PATH
 
 XUSE_NEW_BUILD_SYSTEM=true XBUILD=true ./obuildfactory/openjdk8/linux/standalone-job.sh
+
+DIR="/home/dalex/project/TCJDK"
+
+rm -rf ${DIR}/j2re-image
+tar -xjf ${DIR}/OBF_DROP_DIR/openjdk8/j2re-image-x86_64-*.tar.bz2
--- test.sh
+++ test.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+DIR="/home/dalex/project/TCJDK"
 
-rm -rf /home/dalex/project/JDK/j2re-image
-tar -xjf /home/dalex/project/JDK/OBF_DROP_DIR/openjdk8/j2re-image-x86_64-u82-b00-20160205.tar.bz2
+#rm -rf ${DIR}/j2re-image
+#tar -xjf ${DIR}/OBF_DROP_DIR/openjdk8/j2re-image-x86_64-*.tar.bz2
 
-export JAVA_HOME=/home/dalex/project/JDK/j2re-image
+export JAVA_HOME=${DIR}/j2re-image
+export PATH=$JAVA_HOME/bin:$PATH
 
 java -version
